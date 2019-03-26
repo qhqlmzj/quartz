e98ccdfbd5ffe787575ce72c7723940e2dd9bce6
58GANJI-CORP\mazijun <mazijun@58.com>
2019-03-26 16:56:52 +0800

    add

--- src/main/java/com/mascode/quartz/logic/MonthJob.java
+++ src/main/java/com/mascode/quartz/logic/MonthJob.java
@@ -1,26 +1,29 @@
 package com.mascode.quartz.logic;
 
+import com.mascode.quartz.structure.annotation.SchedulerBinding;
 import com.mascode.quartz.structure.impl.JobBindingAdapter;
 import com.mascode.quartz.structure.po.JobInfo;
-import com.mascode.quartz.structure.annotation.SchedulerBinding;
 import com.mascode.quartz.structure.po.TriggerInfo;
-import org.quartz.JobKey;
-import org.quartz.TriggerKey;
 
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
+@SchedulerBinding
 public class MonthJob extends JobBindingAdapter {
 
     @Override
     protected JobInfo buildJobInfo() {
         JobInfo jobInfo = new JobInfo();
         jobInfo.setJob(Say.class);
+        jobInfo.setJobName("job2");
+        jobInfo.setGroupName("group1");
         return jobInfo;
     }
 
     @Override
     protected TriggerInfo buildTriggerInfo() {
         TriggerInfo triggerInfo = new TriggerInfo();
+        triggerInfo.setTriggerName("trigger2");
+        triggerInfo.setGroupName("group");
         triggerInfo.setScheduleBuilder(simpleSchedule().withIntervalInSeconds(1).repeatForever());
         return triggerInfo;
     }
--- src/main/java/com/mascode/quartz/logic/Say.java
+++ src/main/java/com/mascode/quartz/logic/Say.java
@@ -6,6 +6,6 @@ import org.quartz.JobExecutionException;
 
 public class Say implements Job {
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("say");
+        System.out.println(context.getJobDetail().getKey());
     }
 }
